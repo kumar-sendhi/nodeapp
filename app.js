93a9692e76bb249e0ce0669bce26ed1d292f9c1f
kumar <kumar.sendhi@outlook.com>
2022-09-30 18:51:16 -0400

    morgan

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
+
+//app.use(morgan('combined'));
+app.use(morgan('tiny'));
 
 app.get("/",(req,res)=>{
     res.send('Hello from my app')
