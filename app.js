d2e67ebc1e2e52ba91952c6d70b791d673020e55
kumar <kumar.sendhi@outlook.com>
2022-09-30 18:57:54 -0400

    static

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path')
 
 const app = express();
 
 //app.use(morgan('combined'));
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,'/public/')));
 
 app.get("/",(req,res)=>{
     res.send('Hello from my app')
