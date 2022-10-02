a3148f8e4c9bd176b465fc1b6c7363f63f0d68b6
kumar <kumar.sendhi@outlook.com>
2022-10-02 18:16:08 -0400

    configure passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
 
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const sessionRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 const authRouter = require('./src/routers/authRouter');
@@ -19,6 +23,11 @@ app.use(express.static(path.join(__dirname,'/public/')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({secret: 'globomantics'}));
+
+
+require('./src/config/passport.js')(app)
 
 app.set('views','./src/views');
 
