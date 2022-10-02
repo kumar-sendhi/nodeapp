105e5fb15a6e2ed44a4e03c6243d175c52b5a27f
kumar <kumar.sendhi@outlook.com>
2022-10-02 16:41:49 -0400

    inserting sessions to database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path')
 
 const sessionRouter = require('./src/routers/sessionsRouter');
+const adminRouter = require('./src/routers/adminRouter');
 const PORT = process.env.PORT || 3000
 const app = express();
 
@@ -19,7 +20,9 @@ app.set('view engine','ejs');
 
 
 
-app.use('/session', sessionRouter)
+app.use('/session', sessionRouter);
+app.use('/admin',adminRouter);
+
 
 app.get("/",(req,res)=>{
     res.render('index', {title: 'Globomantics', data :['a','b','c']});
---
+++ src/routers/adminRouter.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const sessions = require('../data/sessions.json');
+const debug = require('debug')('app:adminRouter');
+const mongodb=require('mongodb')
+const adminRouter = express.Router();
+
+
+adminRouter.route('/').get((req,res)=>{
+    const url = 'mongodb+srv://kumarsendhi:vj54gl9ewRJMWbBR@cluster0.uylginj.mongodb.net/?retryWrites=true&w=majority'
+    const dbName = 'globomantics';
+
+    (async function mongo(){
+        let client;
+        try{
+            client = await mongodb.MongoClient.connect(url);
+            debug('connected to the mongo DB');
+
+            const db = client.db(dbName);
+
+            const response = await db.collection('sessions').insertMany(sessions);
+            res.json(response);
+
+        }
+        catch(error){
+            debug(error.stack)
+        }
+    }())
+    mongodb.MongoClient
+})
+
+
+
+
+module.exports = adminRouter;
