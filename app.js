390215f52ac056acc28e4d31ae89b9de302da287
kumar <kumar.sendhi@outlook.com>
2022-10-02 17:36:48 -0400

    creating auth routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require('path')
 
 const sessionRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
+const authRouter = require('./src/routers/authRouter');
+
+
 const PORT = process.env.PORT || 3000
 const app = express();
 
@@ -13,6 +16,9 @@ const app = express();
 //app.use(morgan('combined'));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public/')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.set('views','./src/views');
 
@@ -22,7 +28,7 @@ app.set('view engine','ejs');
 
 app.use('/session', sessionRouter);
 app.use('/admin',adminRouter);
-
+app.use('/auth',authRouter);
 
 app.get("/",(req,res)=>{
     res.render('index', {title: 'Globomantics', data :['a','b','c']});
---
+++ src/routers/authRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const debug = require('debug')('app:sessionRouter');
+const mongodb=require('mongodb')
+const authRouter = express.Router();
+
+
+authRouter.route('/signUp').post((req,res)=>{
+    res.json(req.body);
+})
+
+
+module.exports = authRouter;
